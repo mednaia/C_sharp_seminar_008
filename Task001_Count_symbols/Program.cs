c2ed2e1b9b5522dadc8ba79e7d4205df65066384
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-06-11 21:40:26 +0300

    Task001 count symbols in text_unworked

---
+++ Task001_Count_symbols/Program.cs
@@ -0,0 +1,18 @@
+﻿// Из текста взять каждый символ и посчитать сколько раз он встречается и его % встречаемости
+Console.Write("Write the text: ");
+string text = Console.ReadLine() ?? "0";
+for (int i = 0; i < text.Length; i++)
+{
+    char symbol = text[i];
+    int count = 1;
+    for (int j = i+1; j < text.Length; j++)
+    {
+        if (symbol==text[j])
+        {
+            count++; 
+            text = text.Remove(text[j], 1); //Не работает
+            j--;
+        }
+    }
+    Console.WriteLine($"Symbol {symbol} appears in the text {count} times.");
+}
