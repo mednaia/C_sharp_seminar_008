d7387676003537410fdc4e5ea853f58c3f4a3007
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-06-11 23:39:44 +0300

    Task 001 var 2 with functions

---
+++ Task001_var2_count_symbols/Program.cs
@@ -0,0 +1,42 @@
+﻿// Составить частотный словарь элементов двумерного массива
+//Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+
+int CountCharsInString(string text, char symbol)
+{
+    int count = 0;
+    for (int i = 0; i < text.Length; i++)
+    {
+        if (text[i] == symbol)
+        {
+            count++;
+        }
+
+    }
+    return count;
+}
+string UniqueCharsInString(string text)
+{
+    string newText = string.Empty;
+    for (int i = 0; i < text.Length; i++)
+    {
+        bool check = false;
+        for (int j = i + 1; j < text.Length; j++)
+        {
+            if (text[i] == text[j]) check = true;
+        }
+        if (!check) newText = newText + $"{text[i]}";
+    }
+    return newText;
+}
+Console.Clear();
+Console.WriteLine("Write the text:");
+string text = Console.ReadLine() ?? " ";
+double size = text.Length;
+double percent = 0;
+string newText = UniqueCharsInString(text);
+for (int i = 0; i < newText.Length; i++)
+{
+    int count = CountCharsInString(text, newText[i]);
+    percent = count / size * 100;
+    Console.WriteLine($"Symbol {newText[i]} is in the text {count} times. Percent = {Math.Round(percent, 2)} %");
+}
---
+++ Task001_var3_count_symbols/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+Console.Write("Write the text: ");
+string text = Console.ReadLine() ?? "0";
+text = text.ToLower();
+string collection = "";
+for(int i = 0; i < text.Length; i++)
+{
+    if( !collection.Contains(text[i]))
+        collection += text[i];
+}
+string [,] matrix = new string [collection.Length, 2];
+
+for(int i = 0; i < collection.Length; i++)
+{
+    matrix[i,0] = Convert.ToString(collection[i]);
+    matrix[i,1] = "0";
+}
+
+for(int i = 0; i < text.Length; i++)
+{
+    for(int j = 0; j < collection.Length; j++)
+        if(text[i] == collection[j])
+        {
+            matrix[j,1] = Convert.ToString(Convert.ToInt32(matrix[j,1])+1);
+        }
+}
+for(int j = 0; j < collection.Length; j++)
+    Console.WriteLine($"Symbol {matrix[j,0]} appears in the text {matrix[j,1]} times.");
